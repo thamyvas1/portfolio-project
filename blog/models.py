3c1545e2a3e4811dd89aa11d870f6ddaf33bdd3d
Thamyres <thamyres@live.com>
2021-10-04 15:47:58 -0300

    Bootstrap and jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog Models
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = image = models.ImageField(upload_to='images/')
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
